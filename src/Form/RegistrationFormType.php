57f1b320f5c0810e7624122b8349d73392f34b65
Franck-dev-hub <franck.spadotto@yahoo.com>
2026-05-25 19:01:43 +0200

    fix(form): 12 char long password require to match NIST recommendations

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -36,10 +36,9 @@ class RegistrationFormType extends AbstractType
                         message: 'Please enter a password',
                     ),
                     new Length(
-                        min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        min: 12,
                         max: 4096,
+                        minMessage: 'Your password should be at least {{ limit }} characters',
                     ),
                 ],
             ])
